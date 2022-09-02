a82a65561b41c14887c08df88a5967750cecdba0
Edmund Chan <edmundcwm@gmail.com>
2022-09-02 16:14:01 +0800

    Add JSDoc

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,9 @@
+/**
+ * Retrieve emoji flag based on country code
+ *
+ * @param {string} countryCode Country Code
+ * @return {string} The given content with some characters escaped.
+ */
 export function getEmojiFlag( countryCode ) {
 	return String.fromCodePoint(
 		...countryCode
