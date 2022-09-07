a9f4699bc94df3d19e24fdeb69de502bf924fbe1
Edmund Chan <edmundcwm@gmail.com>
2022-09-07 16:08:20 +0800

    Add block props to Save

--- src/save.js
+++ src/save.js
@@ -1,11 +1,17 @@
 /**
+ * WordPress dependencies
+ */
+import { useBlockProps } from '@wordpress/block-editor';
+
+/**
  * Internal dependencies
  */
 import Preview from './preview';
 
 export default function Save( { attributes } ) {
+	const blockProps = useBlockProps.save();
 	return (
-		<div>
+		<div { ...blockProps }>
 			<Preview { ...attributes } />
 		</div>
 	);
